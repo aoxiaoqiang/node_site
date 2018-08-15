7e5f26d3093387b66ccfa113b0506603af31e701
敖晓强 <aoxiaoqiang@163.com>
2018-08-16 01:42:36 +0800

    feat:

--- src/app.js
+++ src/app.js
@@ -55,9 +55,7 @@ app.use( (req, res, next) => {
 
 // 首页
 app.get('/', (req, res) => {
-  res.redirect({
-    url: '/image/web-design.png'
-  })
+  res.redirect('/image/web-design.png');
   // const loginUser = req.session.loginUser;
   // const isLogined = !!loginUser;
 
